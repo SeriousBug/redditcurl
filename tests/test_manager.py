66b414b85d2d33061c7848d2460ca2925c824cd4
Kaan Genç <SeriousBug@Gmail.com>
2015-03-13 23:57:05 +0200

    Added tests for the update_new function.

--- tests/test_manager.py
+++ tests/test_manager.py
@@ -1,3 +1,5 @@
+import gzip
+import json
 from unittest import mock
 from tests import test_base
 from redditcurl import manager
@@ -35,3 +37,18 @@ class TestManageDownload(test_base.EnterTemp):
     def test_failing_download(self):
         self.assertEqual(manager.manage_download(test_links["fail"], "path", "file"),
             (test_links["fail"], False))
+
+
+class TestUpdateNew(test_base.EnterTemp):
+    def test_write_new(self):
+        # Test with no existing file
+        manager.update_new(list(test_base.test_links.values()), ".downloaded.gz")
+        with gzip.open(".downloaded.gz", "rb") as file:
+            self.assertEqual(list(test_base.test_links.values()), json.loads(file.read().decode("utf-8")))
+
+    def test_write_existing(self):
+        # Test with an existing file
+        manager.update_new(list(test_links.values())[:3], ".downloaded.gz")
+        manager.update_new(list(test_links.values())[3:], ".downloaded.gz")
+        with gzip.open(".downloaded.gz", "rb") as file:
+            self.assertEqual(list(test_links.values()), json.loads(file.read().decode("utf-8")))
